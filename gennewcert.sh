7ecde36514d8e272bc0b9c1d6f5c444de36ef369
Antoine Chabert <antitoine@users.noreply.github.com>
2014-05-19 19:10:49 +0000

    Add script to make new certificat by the CA Antoine CHABERT

---
+++ gennewcert.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Generation of a new SSL certificat signed by the CA Antoine CHABERT
+
+CADIR="/etc/ssl/ca/"
+SSLDIR="/etc/ssl/"
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+case "$1" in
+    -n | --name)
+        cd ${CADIR}
+        mv certs/${2}.pem old/
+        mv private/${2}.key old/
+        mv req/${2}.req old/
+        openssl genrsa -aes256 -out private/${2}.key 2048
+        openssl req -sha256 -new -key private/${2}.key -out req/${2}.req -config openssl.cnf
+        openssl ca -days 365 -in req/${2}.req -out certs/${2}.pem -config ./openssl.cnf
+        cp certs/${2}.pem ${SSLDIR}certs/
+        cp private/${2}.key ${SSLDIR}private/
+        ;;
+    *)
+        echo "Usage: $0 {-n|--name} CERTNAME"
+        ;;
+esac
